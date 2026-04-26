7f072b3c610402dfef9704971ac558543741aeb7
Evgenii <moreoline@gmail.com>
2026-04-26 18:38:26 +1000

    Пытался запустить сервис и бд

---
+++ services/auth-service/src/main.py
@@ -0,0 +1,22 @@
+from fastapi import FastAPI
+import os
+from sqlalchemy import create_api_engine, text
+
+app = FastAPI()
+
+# Берем URL базы из переменных окружения (которые пропишем в docker-compose)
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+@app.get("/")
+def read_root():
+    return {"status": "Service is running!"}
+
+@app.get("/test-db")
+def test_db():
+    try:
+        engine = create_api_engine(DATABASE_URL)
+        with engine.connect() as connection:
+            connection.execute(text("SELECT 1"))
+        return {"status": "Database connection successful!"}
+    except Exception as e:
+        return {"status": "Error", "message": str(e)}
